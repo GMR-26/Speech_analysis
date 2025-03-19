85c994af86b3bf10bee343e2184c212cf18e1252
Christo Martin <christomartinog@gmail.com>
2025-03-19 20:22:21 +0530

    Update forms.py
    Updated forms.py to make sure only audio files with mp3 and wav format can be uploaded

--- speech_analysis/analysis/forms.py
+++ speech_analysis/analysis/forms.py
@@ -2,3 +2,10 @@ from django import forms
 
 class AudioFileForm(forms.Form):
     audio_file = forms.FileField()
+
+from django.core.validators import FileExtensionValidator
+
+class AudioFileForm(forms.Form):
+    audio_file = forms.FileField (
+        validators=[FileExtensionValidator(allowed_extensions=['wav', 'mp3'])]
+    )
